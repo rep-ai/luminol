002a3d27527730a371038041d579d8f8a68b7bae
Aaron Manning <aaron.manning613@gmail.com>
2021-07-05 14:21:53 -0300

    derivative detector updated to use absolute value instead of std

--- src/luminol/algorithms/anomaly_detector_algorithms/derivative_detector.py
+++ src/luminol/algorithms/anomaly_detector_algorithms/derivative_detector.py
@@ -19,11 +19,16 @@ from luminol.modules.time_series import TimeSeries
 
 class DerivativeDetector(AnomalyDetectorAlgorithm):
 
-    '''
+    """
     Derivative Algorithm.
     This method is the derivative version of Method 1.
     Instead of data point value, it uses the derivative of the data point.
-    '''
+
+    AM 2021: This method has been updated to use absolute values of the derivative,
+    rather than a score relative to the data set's standard deviation. Human motion 
+    cannot exceed certain speeds, so setting an upper limit for position data is reasonable.
+    """
+
     def __init__(self, time_series, baseline_time_series=None, smoothing_factor=None):
         """
         Initializer
@@ -31,8 +36,10 @@ class DerivativeDetector(AnomalyDetectorAlgorithm):
         :param TimeSeries baseline_time_series: baseline TimeSeries.
         :param float smoothing_factor: smoothing factor.
         """
-        super(DerivativeDetector, self).__init__(self.__class__.__name__, time_series, baseline_time_series)
-        self.smoothing_factor = (smoothing_factor or DEFAULT_DERI_SMOOTHING_FACTOR)
+        super(DerivativeDetector, self).__init__(
+            self.__class__.__name__, time_series, baseline_time_series
+        )
+        self.smoothing_factor = smoothing_factor or DEFAULT_DERI_SMOOTHING_FACTOR
         self.time_series_items = self.time_series.items()
 
     def _compute_derivatives(self):
@@ -46,7 +53,8 @@ class DerivativeDetector(AnomalyDetectorAlgorithm):
                 pre_timestamp = pre_item[0]
                 pre_value = pre_item[1]
                 td = timestamp - pre_timestamp
-                derivative = (value - pre_value) / td if td != 0 else value - pre_value
+                derivative = (value - pre_value) / \
+                    td if td != 0 else value - pre_value
                 derivative = abs(derivative)
                 derivatives.append(derivative)
         # First timestamp is assigned the same derivative as the second timestamp.
@@ -60,11 +68,16 @@ class DerivativeDetector(AnomalyDetectorAlgorithm):
         """
         anom_scores = {}
         self._compute_derivatives()
-        derivatives_ema = utils.compute_ema(self.smoothing_factor, self.derivatives)
+        derivatives_ema = utils.compute_ema(
+            self.smoothing_factor, self.derivatives)
         for i, (timestamp, value) in enumerate(self.time_series_items):
-            anom_scores[timestamp] = abs(self.derivatives[i] - derivatives_ema[i])
-        stdev = numpy.std(list(anom_scores.values()))
-        if stdev:
-                for timestamp in anom_scores.keys():
-                    anom_scores[timestamp] /= stdev
+            anom_scores[timestamp] = abs(
+                self.derivatives[i] - derivatives_ema[i])
+
+        # AM 2021: edited to remove relative comaprison to standard dev.
+
+        # stdev = numpy.std(list(anom_scores.values()))
+        # if stdev:
+        #         for timestamp in anom_scores.keys():
+        #             anom_scores[timestamp] /= stdev
         self.anom_scores = TimeSeries(self._denoise_scores(anom_scores))
